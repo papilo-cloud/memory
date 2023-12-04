e2ff93c2d91778c2ecd6797fae424077c578f416
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-12-04 15:35:32 -0800

    done with header UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { Game } from './components/Game'
 import ProviderContext, { useTasks } from './hooks/useContext';
 import { Menu } from './components/Menu';
 import { Modal } from './components/modal/Modal';
 
 function App() {
-  const {grid} = useTasks()
-  console.log(grid)
+
   return (
       <ProviderContext >
         <Menu />
-        <Game />
         {/* <Modal /> */}
       </ProviderContext>
   )
